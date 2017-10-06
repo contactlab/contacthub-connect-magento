bf9ba440cd80377a302cafefeefd272465c9c830
Luca Osti <plutonio@gmail.com>
2017-10-06 16:56:15 +0200

    Prepare AddToCompare event data before saving to DB

--- app/code/community/Contactlab/Hub/Model/Event/AddToCompare.php
+++ app/code/community/Contactlab/Hub/Model/Event/AddToCompare.php
@@ -1,31 +1,26 @@
 <?php
 class Contactlab_Hub_Model_Event_AddToCompare extends Contactlab_Hub_Model_Event
 {
-	protected function _assignData()
-	{				
-		if(!$this->_getSid())
-		{
-			return;
-		}
-		$product = $this->getEvent()->getProduct();				
-		$eventData = array(
-						'product_id' => $product->getId()
-					);
-		$this->setName('addedCompare')
-			->setModel('addToCompare')
-			->setEventData(json_encode($eventData));
-		
-		return parent::_assignData();
-	}
-	
-	protected function _composeHubEvent()
-	{
-		if(!$this->_eventForHub)
-		{
-			$this->_eventForHub = new stdClass();
-		}
-		$eventData = json_decode($this->getEventData());
-		$this->_eventForHub->properties = $this->_getObjProduct($eventData->product_id);
-		return parent::_composeHubEvent();
-	}
+    protected function _assignData()
+    {
+        if (!$this->_getSid()) {
+            return;
+        }
+
+        $product = $this->getEvent()->getProduct();
+        $this->setName('addedCompare')
+            ->setModel('addToCompare')
+            ->setEventData(json_encode($this->_toHubProduct($product)));
+
+        return parent::_assignData();
+    }
+    
+    protected function _composeHubEvent()
+    {
+        if (!$this->_eventForHub) {
+            $this->_eventForHub = new stdClass();
+        }
+        $this->_eventForHub->properties = json_decode($this->getEventData());
+        return parent::_composeHubEvent();
+    }
 }
