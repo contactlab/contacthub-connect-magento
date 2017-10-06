9ad1364a61e0bd3adb313581d6d59f14031a2e31
Luca Osti <plutonio@gmail.com>
2017-10-06 16:51:42 +0200

    Fixed wrong product ID in event
    While removing a product from the compare block, the object in the event is actually a Mage_Catalog_Model_Product_Compare_Item, and its ID does not coincide with the associated product ID

--- app/code/community/Contactlab/Hub/Model/Event/RemoveToCompare.php
+++ app/code/community/Contactlab/Hub/Model/Event/RemoveToCompare.php
@@ -1,20 +1,21 @@
 <?php
 class Contactlab_Hub_Model_Event_RemoveToCompare extends Contactlab_Hub_Model_Event_AddToCompare
 {
-	protected function _assignData()
-	{				
-		if(!$this->_getSid())
-		{
-			return;
-		}	
-		$product = $this->getEvent()->getProduct();				
-		$eventData = array(
-						'product_id' => $product->getId()
-					);
-		$this->setName('removedCompare')
-			->setModel('removeToCompare')
-			->setEventData(json_encode($eventData));
-		
-		return Contactlab_Hub_Model_Event::_assignData();
-	}
+    protected function _assignData()
+    {
+        if (!$this->_getSid()) {
+            return;
+        }
+
+        // This is actually a Mage_Catalog_Model_Product_Compare_Item object
+        $product = $this->getEvent()->getProduct();
+        $eventData = array(
+                        'product_id' => $product->getProductId()
+                    );
+        $this->setName('removedCompare')
+            ->setModel('removeToCompare')
+            ->setEventData(json_encode($eventData));
+        
+        return Contactlab_Hub_Model_Event::_assignData();
+    }
 }
