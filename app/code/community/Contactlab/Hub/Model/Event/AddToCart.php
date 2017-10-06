d9a5439036ce91990192f88ad6f279eae06270a3
Luca Osti <plutonio@gmail.com>
2017-10-06 16:04:47 +0200

    Prepare cart event data before saving to DB

--- app/code/community/Contactlab/Hub/Model/Event/AddToCart.php
+++ app/code/community/Contactlab/Hub/Model/Event/AddToCart.php
@@ -1,36 +1,31 @@
 <?php
 class Contactlab_Hub_Model_Event_AddToCart extends Contactlab_Hub_Model_Event
 {
-	protected function _assignData()
-	{		
-		if(!$this->_getSid())
-		{
-			return;
-		}
-		$item = $this->getEvent()->getQuoteItem();
-		$product = $this->getEvent()->getProduct();		
-		if($product)
-		{
-			$eventData = array(
-							'product_id' => $product->getId(),
-							'qty' => $item->getQty()
-						);
-			$this->setName('addedProduct')
-				->setModel('addToCart')
-				->setEventData(json_encode($eventData));
-		}
-		return parent::_assignData();
-	}
-	
-	protected function _composeHubEvent()
-	{		
-		if(!$this->_eventForHub)
-		{
-			$this->_eventForHub = new stdClass();
-		}		
-		$eventData = json_decode($this->getEventData());		
-		$this->_eventForHub->properties = $this->_getObjProduct($eventData->product_id);
-		$this->_eventForHub->properties->quantity = $eventData->qty;
-		return parent::_composeHubEvent();
-	}
+    protected function _assignData()
+    {
+        if (!$this->_getSid()) {
+            return;
+        }
+        $item = $this->getEvent()->getQuoteItem();
+        $product = $this->getEvent()->getProduct();
+
+        if ($product) {
+            $eventData = $this->_toHubProduct($product);
+            $eventData->quantity = $item->getQty();
+            
+            $this->setName('addedProduct')
+                ->setModel('addToCart')
+                ->setEventData(json_encode($eventData));
+        }
+        return parent::_assignData();
+    }
+    
+    protected function _composeHubEvent()
+    {
+        if (!$this->_eventForHub) {
+            $this->_eventForHub = new stdClass();
+        }
+        $this->_eventForHub->properties = json_decode($this->getEventData());
+        return parent::_composeHubEvent();
+    }
 }
