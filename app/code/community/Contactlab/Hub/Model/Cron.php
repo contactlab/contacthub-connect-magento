5daf2d60fc9d389c17cdc2037fbd693068cf08ea
Mladen Ilic <mladen@stuntcoders.com>
2018-03-05 15:44:58 +0100

    Fix code style

--- app/code/community/Contactlab/Hub/Model/Cron.php
+++ app/code/community/Contactlab/Hub/Model/Cron.php
@@ -1,20 +1,22 @@
 <?php 
 class Contactlab_Hub_Model_Cron extends Mage_Core_Model_Abstract
-{	
+{
 	protected $_helper = null;
-	
-	private function _helper() {
+
+	private function _helper()
+    {
 		if ($this->_helper == null) {
 			$this->_helper = Mage::helper('contactlab_hub');
 		}
 		return $this->_helper;
 	}
-	
-	private function _getConfig($key) {
+
+	private function _getConfig($key)
+    {
 		return $this->_helper()->getConfigData($key);
 	}
-	
-	private function _isExportPreviousCustomersEnabled() 
+
+	private function _isExportPreviousCustomersEnabled()
 	{
 		$enable = false;
 		$allStores = Mage::app()->getStores();
@@ -24,71 +26,71 @@ class Contactlab_Hub_Model_Cron extends Mage_Core_Model_Abstract
 		}
 		return $enable;
 	}
-	
-	private function _isImportSubscribersEnabled() {
+
+	private function _isImportSubscribersEnabled()
+    {
 		return $this->_getConfig('cron_subscribers/enabled')?true:false;
 	}
-	
+
 	/**
 	 * Log function call.
-	 * @param String $functionName
-	 * @param String $storeId
+	 * @param string $functionName
+	 * @param string|boolean $storeId
 	 */
 	public function logCronCall($functionName, $storeId = false)
 	{
-		Mage::helper('contactlab_hubcommons')
-		->logCronCall(
-				"Contactlab_Subscribers_Model_Cron::$functionName", $storeId
-				);
+		Mage::helper('contactlab_hubcommons')->logCronCall(
+		    "Contactlab_Subscribers_Model_Cron::$functionName",
+            $storeId
+        );
 	}
-	
+
 	public function exportEvents()
-	{		
-		$this->logCronCall("addExportEventsQueue");	
+	{
+		$this->logCronCall("addExportEventsQueue");
 		return Mage::getModel("contactlab_hubcommons/task")
 			->setTaskCode("ExportEventsTask")
 			->setModelName('contactlab_hub/task_exportEvents')
 			->setDescription('Export Events')
 			->save();
 	}
-	
+
 	public function exportPreviousCustomers()
-	{					
-		if (!$this->_isExportPreviousCustomersEnabled() )
-		{
+	{
+		if (!$this->_isExportPreviousCustomersEnabled() ) {
 			return;
-		}		
-	
-		$this->logCronCall("addExportPreviousCustomersQueue");		
-		return Mage::getModel("contactlab_hubcommons/task")		
+		}
+
+		$this->logCronCall("addExportPreviousCustomersQueue");
+		return Mage::getModel("contactlab_hubcommons/task")
 			->setTaskCode("ExportPreviousCustomersTask")
 			->setModelName('contactlab_hub/task_exportPreviousCustomers')
-			->setDescription('Export Previous Coustomers')	
+			->setDescription('Export Previous Coustomers')
 			->save();
 	}
-	
+
 	public function getAbandonedCarts()
 	{
 		Mage::getModel("contactlab_hub/exporter_abandonedCarts")->export();
 	}
-	
+
 	/**
 	 * Add importer task to queue.
 	 */
-	public function importSubscribers() 
+	public function importSubscribers()
 	{
-		if (!$this->_isImportSubscribersEnabled()) 
-		{
+		if (!$this->_isImportSubscribersEnabled()) {
 			return;
-		}		
+		}
+
 		$this->logCronCall("addImportSubscribersQueue", $storeId);
-		return Mage::getModel("contactlab_hubcommons/task")		
+		return Mage::getModel("contactlab_hubcommons/task")
 			->setTaskCode("ImportSubscribersTask")
 			->setModelName('contactlab_hub/task_importSubscribers')
 			->setDescription('Import Subscribers')
 			->save();
 	}
-	
+
 	/**
 	 * Clean Old Events
 	 */
